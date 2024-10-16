4c4697a16bcd004ceed46e859f8b41c91a3857bd
Gongmyeong-Company <gmco_ceo@daum.net>
2024-10-16 17:49:29 +0900

    2.0

---
+++ front/src/screens/AuthHomeScreen.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Button, SafeAreaView, StyleSheet, View} from 'react-native';
+import {authNaviagtions} from '../constants';
+import { StackScreenProps } from '@react-navigation/stack';
+import { AuthStackParamList } from '../navigations/AuthStackNavigator';
+
+type AuthHomeScreenProps = StackScreenProps<AuthStackParamList, typeof authNaviagtions.AUTH_HOME>;
+
+function AuthHomeScreen({navigation}: AuthHomeScreenProps){
+  return (
+    <SafeAreaView>
+      <View>
+        <Button
+          title="로그인화면으로 이동"
+          onPress={() => navigation.navigate(authNaviagtions.LOGIN)}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default AuthHomeScreen;
